696dcb557b29d894dd1651b3c331bd8209ad89d9
Marat Baktybekov <mrtbaktybek@gmail.com>
2026-08-26 22:32:50 +0900

    Configure GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,31 @@ function ArticlesLayout() {
   );
 }
 function App() {
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <ArticlesLayout></ArticlesLayout>,
+        children: [
+          {
+            index: true,
+            element: <ArticlesPages></ArticlesPages>,
+          },
+          {
+            path: "article/:slug",
+            element: <Article></Article>,
+          },
+          {
+            path: "*",
+            element: <NotFound></NotFound>,
+          },
+        ],
+      },
+    ],
     {
-      path: "/",
-      element: <ArticlesLayout></ArticlesLayout>,
-      children: [
-        {
-          index: true,
-          element: <ArticlesPages></ArticlesPages>,
-        },
-        {
-          path: "/article/:slug",
-          element: <Article></Article>,
-        },
-        {
-          path: "*",
-          element: <NotFound></NotFound>,
-        },
-      ],
+      basename: "/SimpleBlogWithReact-4.13",
     },
-  ]);
+  );
   return (
     <>
       <RouterProvider router={router} />
